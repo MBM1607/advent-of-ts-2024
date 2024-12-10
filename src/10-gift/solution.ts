a3f76b7ecd45c43edd489eba69f17eccdb112cd8
mbm_1607 <muhammadkhan1607@gmail.com>
2024-12-10 11:11:09 +0500

    ✨ Solve Day 10

---
+++ src/10-gift/solution.ts
@@ -0,0 +1,11 @@
+export enum Gift {
+	Coal,
+	Train,
+	Bicycle,
+	Traditional,
+	SuccessorToTheNintendoSwitch = 0b100,
+	TikTokPremium = 0b1000,
+	Vape = 0b10000,
+	OnTheMove = 0b11010,
+	OnTheCouch = 0b11100,
+}
